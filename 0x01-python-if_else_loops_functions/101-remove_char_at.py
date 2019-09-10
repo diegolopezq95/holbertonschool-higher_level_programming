5de71c79f87336fcb1c69e5e79a2e26f96b82f43
diegolopezq95 <927@holbertonschool.com>
2019-09-10 20:39:52 +0000

    remove n character

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,8 +2,8 @@
 def remove_char_at(str, n):
     strcpy = ""
     for i in range(len(str)):
+        strcpy = str[:n] + str[n + 1:]
         if n >= 0 and n < len(str):
-            strcpy = str[:n] + str[n + 1:]
             return strcpy
         else:
             return str
