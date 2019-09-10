c1e094990a66f7a48af4710a534823d0600155b5
diegolopezq95 <927@holbertonschool.com>
2019-09-10 17:23:48 +0000

    string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
         c = ord(str[i])
-        if str[i].islower():        
+        if str[i].islower():
             c = c - 32
         print('{}'.format(chr(c)), end="")
     print()
