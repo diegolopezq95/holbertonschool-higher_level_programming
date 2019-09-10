6aa282981a5f54cc689371e1ac908e449def6627
diegolopezq95 <927@holbertonschool.com>
2019-09-10 15:13:41 +0000

    [Fix] pep8 fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdigit = abs(number) % 10
-if lastdigit > 5 and number > 0:
-    print('Last digit of', "{:d} is {:d} and is greater than 5".format(number, lastdigit))
-elif lastdigit == 0 and number > 0:
-    print('Last digit of', "{:d} is {:d} and is 0".format(number, lastdigit))
-elif lastdigit < 6 and lastdigit > 0 and number > 0:
-    print('Last digit of', "{:d} is {:d} and is less than 6 and not 0".format(number, lastdigit))
+last = abs(number) % 10
+str = "Last digit of"
+print("{}".format(str), end=" ")
+if last > 5 and number > 0:
+    print("{:d} is {:d} and is greater than 5".format(number, last))
+elif last == 0 and number > 0:
+    print("{:d} is {:d} and is 0".format(str, number, last))
+elif last < 6 and last > 0 and number > 0:
+    print("{:d} is {:d} and is less than 6 and not 0".format(number, last))
 else:
-    print('Last digit of', "{:d} is {:d} and is less than 6 and not 0".format(number, lastdigit * -1))
+    print("{:d} is {:d} and is less than 6 and not 0".format(number, last*-1))
