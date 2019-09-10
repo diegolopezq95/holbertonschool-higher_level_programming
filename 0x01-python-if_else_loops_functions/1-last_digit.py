c20f506b7f486cd87c76462c202dfbc02979b1bd
diegolopezq95 <927@holbertonschool.com>
2019-09-10 17:12:13 +0000

    [Fix] str type error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ str = "Last digit of"
 print("{}".format(str), end=" ")
 if last > 5 and number > 0:
     print("{:d} is {:d} and is greater than 5".format(number, last))
-elif last == 0 and number > 0:
+elif last == 0 and number >= 0:
     print("{:d} is {:d} and is 0".format(number, last))
 elif last < 6 and last > 0 and number > 0:
     print("{:d} is {:d} and is less than 6 and not 0".format(number, last))
