bc243a11ab13eb1dae8e7e6ac3133695bbaaae73
diegolopezq95 <927@holbertonschool.com>
2019-09-17 17:45:44 +0000

    [Fix] if not exist sentence return None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     tuple_s = (sentence)
     length = len(sentence)
     if not sentence: 
-        return (length, tuple_s[0:1])
-    else:
         return None
+    else:
+        return (length, tuple_s[0:1])
