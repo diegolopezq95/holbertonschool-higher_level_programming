00065b90d460bc069f297bc86e9d38b6b7172c5d
diegolopezq95 <927@holbertonschool.com>
2019-09-17 19:08:29 +0000

    delete item at list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     for i in range(len(my_list)):
-        if idx < 0 or idx > len(my_list):
+        if idx < 0 or idx > len(my_list) - 1:
             return my_list
         if i == idx:
-            my_list.remove(idx)
-        elif i == idx:
             my_list.remove(idx)
         else:
             continue
