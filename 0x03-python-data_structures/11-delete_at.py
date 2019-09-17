9e059cfbf094d0b21e094a0848aece187cd3eff5
diegolopezq95 <927@holbertonschool.com>
2019-09-17 19:14:53 +0000

    [Fix] adding pass

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if not len(my_list):
-        return None
-    else:
-        for i in range(len(my_list)):
-            if idx < 0 or idx > len(my_list) - 1:
-                return my_list
-            elif i == idx:
-                my_list.remove(idx)
-            else:
-                continue
-            new_list = my_list.copy()
-        return new_list
+    for i in range(len(my_list)):
+        if idx < 0 or idx > len(my_list) - 1:
+                pass
+        elif i == idx:
+            my_list.remove(idx)
+        else:
+            continue
+    return my_list
