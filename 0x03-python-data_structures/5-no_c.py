643f68fe1de8568eae0499cf7dadce2190c7c4fd
diegolopezq95 <927@holbertonschool.com>
2019-09-17 17:11:59 +0000

    [Fix] pep8 fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
     my_string = list(my_string)
-    for i in my_string:
-        new_string = my_string.copy()
+    new_string = my_string.copy()
+    for i in (new_string):
         if i == 'c':
             new_string.remove('c')
-            return ''.join(new_string)
         elif i == 'C':
             new_string.remove('C')
-            return ''.join(new_string)
         else:
             continue
+    return''.join(new_string)
