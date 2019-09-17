e8ba0cd0dcdc3eca3da77808cb2a7665572d1d78
diegolopezq95 <927@holbertonschool.com>
2019-09-17 17:50:23 +0000

    [Fix] fix index

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if not sentence:
-        return None
+        return (length, None)
     else:
         return (length, sentence[0])
