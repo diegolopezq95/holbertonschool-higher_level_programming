8294e0ebe2925f0c5a0c8d20f75ba4741999aa3d
diegolopezq95 <927@holbertonschool.com>
2019-09-17 20:52:48 +0000

    remove char with translate

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = list(my_string)
-    new_string = my_string.copy()
-    for i in (new_string):
-        if i == 'c':
-            new_string.remove('c')
-        elif i == 'C':
-            new_string.remove('C')
+    for i in (my_string):
+        if i == 'c' or i == 'C':
+            my_string =  my_string.translate({ord(i): None})
         else:
             continue
-    return''.join(new_string)
+    return my_string
