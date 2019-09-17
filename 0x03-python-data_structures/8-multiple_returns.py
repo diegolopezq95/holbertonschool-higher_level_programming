51811866fce170c4a4f775354c3be4c3664fd8ff
diegolopezq95 <927@holbertonschool.com>
2019-09-17 17:57:53 +0000

    [Fix] fix None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if not sentence:
-        sentence[0] = None
+    if sentence:
         return (length, sentence[0])
     else:
-        return (length, sentence[0])
+        return None
