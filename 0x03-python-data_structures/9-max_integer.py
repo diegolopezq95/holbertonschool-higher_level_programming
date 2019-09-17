cf003ebc2014aabac11104177a9cd7fca28f9c37
diegolopezq95 <927@holbertonschool.com>
2019-09-17 18:17:36 +0000

    [Fix] adding empty list condition

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = my_list[0]
-    if not len(my_list):
+    if not my_list:
         return None
     else:
         for i in my_list:
             if i > max:
                 max = i
-                return max
+            return max
