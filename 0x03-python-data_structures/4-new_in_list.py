bfc10b28a5401325a7b7623f59e4d785fa1c95b6
diegolopezq95 <927@holbertonschool.com>
2019-09-17 17:28:21 +0000

    [Fix] length fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     cpy_list = my_list.copy()
     if idx < 0:
         return cpy_list
-    elif idx > len(my_list):
+    elif idx > len(my_list) - 1:
         return cpy_list
     else:
         for i in range(len(cpy_list)):
