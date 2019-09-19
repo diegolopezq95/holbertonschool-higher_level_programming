f49deefbbd1b3b58d1d1e3ad26874019cfa74f7c
diegolopezq95 <927@holbertonschool.com>
2019-09-19 20:16:02 +0000

    complex delete value from dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     new_dictionary = a_dictionary.copy()
     if value:
-        for key, values in new_dictionary.items():
+        for key in new_dictionary:
             if a_dictionary[key] == value:
                 del a_dictionary[key]
         return a_dictionary
