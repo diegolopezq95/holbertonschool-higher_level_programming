7985ca0ea0fb61cfff2919d3f5158a65c89ef83a
diegolopezq95 <927@holbertonschool.com>
2019-09-30 17:33:58 +0000

    [Fix]: adding except inside loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
             i = i + 1
-        print("")
-        return i
-    except IndexError:
-        print("")
-        return i
+        except IndexError:
+            break
+    print("")
+    return i
