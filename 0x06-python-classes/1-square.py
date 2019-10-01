974f2abfeaa11625d4b710eb8244c72163c8b671
diegolopezq95 <927@holbertonschool.com>
2019-10-01 16:54:37 +0000

    Square with private instance attribute: size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This module creates an empty class Square that defines a square
+Example:
+    ./0-main.py test file
+Private instance attribute:
+    size
+"""
+
+
+class Square:
+    """ Empty class Square now with size private attribute
+    """
+    def __init__(self, size):
+        """
+        Instantiation with size
+        Args:
+            size: size of the square
+        """
+        self.__size = size
