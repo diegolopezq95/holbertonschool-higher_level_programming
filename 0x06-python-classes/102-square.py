7c1722f33171db4e90ea0b8ecf61d18e53180ebd
diegolopezq95 <927@holbertonschool.com>
2019-10-01 23:00:54 +0000

    [Fix]: pep8

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -45,12 +45,9 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-
     """
     Comparison methods
     """
-
     def __eq__(self, other):
         return self.size == other.size
 
