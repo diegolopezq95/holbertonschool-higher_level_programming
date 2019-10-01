be1ebad0eee1fc3c16b105e62ffe27f62207d1c3
diegolopezq95 <927@holbertonschool.com>
2019-10-01 16:41:52 +0000

    Square empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module creates an empty class Square that defines a square
+See:
+    ./0-main.py test file
+"""
+class Square:
+    """ Empty class Square using pass
+    """
+    pass
