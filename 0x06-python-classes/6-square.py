735ec671312dc6a4b23e673a9be33162df73537b
diegolopezq95 <927@holbertonschool.com>
2019-10-01 21:22:10 +0000

    [Fix]: fixing conditions of position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,10 +17,6 @@ class Square:
             size: size of the square and is positive.
             position: position of the square
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
 
