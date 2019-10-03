2dcf106eeedb7d7aee024b307521f11ad4b22958
diegolopezq95 <927@holbertonschool.com>
2019-10-03 01:47:37 +0000

    singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -17,15 +17,15 @@ class Node:
             data: data inside the node
             next_node: next node
         """
-        self.__data = data
-        self.__next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
         """
         data getter. Returns the data of the linked list
         """
-        return (self.__size)
+        return (self.__data)
 
     @data.setter
     def data(self, value):
@@ -48,10 +48,9 @@ class Node:
         """
         next_node setter. Sets the next_node of the list
         """
-        if next_node is not None and type(next_node) is not Node:
+        if value is not None and type(value) is not Node:
             raise TypeError("next_node must be a Node object")
-        else:
-            self.__next_node = value
+        self.__next_node = value
 
 class SinglyLinkedList:
     """ Empty class Node
@@ -65,5 +64,31 @@ class SinglyLinkedList:
         self.__head = None
 
     def sorted_insert(self, value):
+        """
+        Inserts a node sorted
+        """
+        tmp = self.__head
         if self.__head is None:
-            self.__head = Node(value)
+            self.__head = Node(value, None)
+        elif self.__head.data > value:
+            self.__head = Node(value, tmp)
+        else:
+            while tmp is not None:
+                if tmp.next_node is None:
+                    tmp.next_node = Node(value)
+                    break
+                if tmp.next_node.data > value:
+                    tmp.next_node = Node(value, tmp.next_node)
+                    break
+                tmp = tmp.next_node
+
+    def __str__(self):
+        """
+        Returns linked list
+        """
+        tmp = self.__head
+        linkedlist = []
+        while tmp is not None:
+            linkedlist.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ("\n".join(linkedlist))
