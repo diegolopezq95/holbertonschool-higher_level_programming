a05fa0507e82f9c01d25cde1c8482535af880218
diegolopezq95 <927@holbertonschool.com>
2019-10-05 05:19:59 +0000

    [Fix]: fixing condition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,11 +10,14 @@ def add_integer(a, b=98):
     """
     Method that adds two numbers (a + b)
     """
-    if isinstance(a, float) or isinstance(b, float):
+    if isinstance(a, float):
         a = int(a)
+    if isinstance(b, float):
         b = int(b)
     if a is None:
         raise TypeError("a must be an integer")
+    if b is None:
+        raise TypeError("b must be an integer")
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
