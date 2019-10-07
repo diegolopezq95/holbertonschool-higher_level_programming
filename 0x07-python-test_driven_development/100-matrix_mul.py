8803ebcb82f429a463dc67be0c940b08535d54c6
diegolopezq95 <927@holbertonschool.com>
2019-10-07 01:13:29 +0000

    [Fix]: adding conditions

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -32,19 +32,19 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_b can't be empty")
 
     for row in m_a:
-        for i in row:
-            if not isinstance(i, int) and not isinstance(i, float):
+        for i_a in row:
+            if not isinstance(i_a, int) and not isinstance(i_a, float):
                 raise TypeError("m_a should contain only integers or floats")
     for row in m_b:
-        for i in row:
-            if not isinstance(i, int) and not isinstance(i, float):
+        for i_b in row:
+            if not isinstance(i_b, int) and not isinstance(i_b, float):
                 raise TypeError("m_b should contain only integers or floats")
 
-    for i in m_a:
-        if len(i) != len(m_a[0]):
+    for i_a in m_a:
+        if len(i_a) != len(m_a[0]):
             raise TypeError("each row of m_a must be of the same size")
-    for i in m_b:
-        if len(i) != len(m_b[0]):
+    for i_b in m_b:
+        if len(i_b) != len(m_b[0]):
             raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
@@ -53,6 +53,6 @@ def matrix_mul(m_a, m_b):
     result = [[0 for col in range(len(m_b[0]))] for row in range(len(m_a))]
     for i in range(len(m_a)):
         for j in range(len(m_b[0])):
-            for k in range(len(m_a[0])):
+            for k in range(len(m_b)):
                 result[i][j] += m_a[i][k] * m_b[k][j]
     return result
