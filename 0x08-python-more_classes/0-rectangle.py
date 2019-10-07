44261760dccc007cd4763dcab33b81c2553a73bd
diegolopezq95 <927@holbertonschool.com>
2019-10-07 16:55:34 +0000

    empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module write an empty class Rectangle
+See:
+    ./0-main.py test file
+"""
+
+
+class Rectangle:
+    """
+    Empty class Rectangle
+    """
+    pass
