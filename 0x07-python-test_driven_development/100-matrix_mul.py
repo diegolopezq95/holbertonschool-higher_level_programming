b9b4c889972b322f4b95b9b9ffd5a49dbb076246
diegolopezq95 <927@holbertonschool.com>
2019-10-07 01:04:04 +0000

    [Fix]: adding tests and fix condition

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -40,11 +40,11 @@ def matrix_mul(m_a, m_b):
             if not isinstance(i, int) and not isinstance(i, float):
                 raise TypeError("m_b should contain only integers or floats")
 
-    for i in range(len(m_a)):
-        if len(m_a[i]) != len(m_a[0]):
+    for i in m_a:
+        if len(i) != len(m_a[0]):
             raise TypeError("each row of m_a must be of the same size")
-    for i in range(len(m_b)):
-        if len(m_b[i]) != len(m_b[0]):
+    for i in m_b:
+        if len(i) != len(m_b[0]):
             raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
@@ -53,6 +53,6 @@ def matrix_mul(m_a, m_b):
     result = [[0 for col in range(len(m_b[0]))] for row in range(len(m_a))]
     for i in range(len(m_a)):
         for j in range(len(m_b[0])):
-            for k in range(len(m_b)):
+            for k in range(len(m_a[0])):
                 result[i][j] += m_a[i][k] * m_b[k][j]
     return result
