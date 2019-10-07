c4199b97a427a8c9aafc0ad95470bd23160d086a
diegolopezq95 <927@holbertonschool.com>
2019-10-07 00:40:37 +0000

    [Fix]: fixing wrong message

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -42,10 +42,10 @@ def matrix_mul(m_a, m_b):
 
     for i in range(len(m_a)):
         if len(m_a[i]) != len(m_a[0]):
-            raise TypeError("each row of m_a must should be of the same size")
+            raise TypeError("each row of m_a must be of the same size")
     for i in range(len(m_b)):
         if len(m_b[i]) != len(m_b[0]):
-            raise TypeError("each row of m_b must should be of the same size")
+            raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
