048e03805b8618601bc5a7a3511fa48ca6811d37
diegolopezq95 <927@holbertonschool.com>
2019-10-08 00:00:35 +0000

    [Fix]: fixing typeerror message

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,7 +38,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
@@ -53,6 +53,6 @@ class Rectangle:
         """
         Perimeter of a rectangle
         """
-        return (self.__width * 2) + (self.__height * 2)
         if self.__width == 0 or self.__height == 0:
             return 0
+        return (self.__width * 2) + (self.__height * 2)
