e60a9bd45b00d16e51b66a36e3be2a289a2ea4ef
diegolopezq95 <927@holbertonschool.com>
2019-10-08 18:13:57 +0000

    Solutions everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module creates a class LockedClass
+See:
+    ./101-main.py test file
+"""
+
+
+class LockedClass:
+    """ Empty class using pass
+    """
+    def __init__(self):
+        pass
+    __slots__ = ('first_name')
