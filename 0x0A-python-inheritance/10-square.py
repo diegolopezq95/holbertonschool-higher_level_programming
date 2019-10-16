5b2698ebd1f9371acc684e83fdf458aeb142ca65
diegolopezq95 <927@holbertonschool.com>
2019-10-16 14:34:39 +0000

    [Fix]: change estructure by importing module

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,48 +6,7 @@ See:
 """
 
 
-class BaseGeometry:
-    """
-    Adding integers validator method
-    """
-    def area(self):
-        """
-        Nothing happens
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate integer
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """
-    Create class Rectangle inherit from BaseGeometry
-    """
-    def __init__(self, width, height):
-        """
-        Instantiation
-        """
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """
-        area() method implemented
-        """
-        return (self.__width * self.__height)
-        return (self.__width * self.__height)
-
-    def __str__(self):
-        return("[Rectangle] {}/{}".format(self.__width, self.__height))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,24 +6,7 @@ See:
 """
 
 
-class BaseGeometry:
-    """
-    Adding integers validator method
-    """
-    def area(self):
-        """
-        Nothing happens
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate integer
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
