fb48701580bfd90c08b80880dacb058b9bc1aa71
diegolopezq95 <927@holbertonschool.com>
2019-10-20 01:07:09 +0000

    JSON string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,14 +2,19 @@
 """ This module create Class Base.
 The “base” of all other classes in this project
 See:
-    ./0-main.py test file
+    ./14-main.py test file
 """
+
+
+import json
+
 
 class Base:
     """
     Base class
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """
         Instantiation
@@ -21,3 +26,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
