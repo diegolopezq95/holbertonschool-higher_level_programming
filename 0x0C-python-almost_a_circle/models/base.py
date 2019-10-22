dc5787f64d1eceb4eb67b49fdd9b0bff24fe0fbf
diegolopezq95 <927@holbertonschool.com>
2019-10-22 22:15:33 +0000

    [Fix]: empty list condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,8 +35,7 @@ class Base:
         """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
