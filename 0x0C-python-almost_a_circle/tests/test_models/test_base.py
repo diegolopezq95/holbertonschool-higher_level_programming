3dd860b7ea43ee4bcbb2f1d419ad2d7c3854978a
diegolopezq95 <927@holbertonschool.com>
2019-10-22 22:32:33 +0000

    More tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -32,3 +32,50 @@ class TestBase(unittest.TestCase):
         i = Base(-89)
         self.assertEqual(-89, i.id)
 
+    def test_string_input(self):
+        """
+        Test a string id
+        """
+        i = Base("Holberton")
+        self.assertEqual("Holberton", i.id)
+
+
+    def test_docs_B(self):
+        """
+        Test for Base class docstring
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_docs_R(self):
+        """
+        Test for Rectabgle class docstring
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_docs_S(self):
+        """
+        Test for Square class docstring
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_to_json_none(self):
+        """
+        Test for to json string empty list
+        """
+        i = Base.to_json_string(None)
+        self.assertEqual("[]", i)
+
+    def test_to_json_string(self):
+        """
+        Test for to json string with args
+        """
+        i = Base.to_json_string([{"John": 23}, {"Betty": 56}])
+        self.assertEqual(str, type(i))
+
+    def test_save_to_file_none(self):
+        """
+        Tests save to file None
+        """
+        Base.save_to_file(None)
+        with open("Base.json", "r") as f:
+            self.assertEqual(f.read(), "[]")
