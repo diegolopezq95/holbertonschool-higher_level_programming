f86e411729a4aeea3203f9989ba1d01ee4db6882
diegolopezq95 <927@holbertonschool.com>
2019-10-22 22:13:03 +0000

    [Fix]: empty list condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,7 +33,7 @@ class Base:
         """
         Returns the JSON string representation of list_dictionaries
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
