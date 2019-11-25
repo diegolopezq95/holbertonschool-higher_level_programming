01a142c181a50af0988c300c4f66e659b0e5481b
diegolopezq95 <927@holbertonschool.com>
2019-11-25 17:16:23 +0000

    lists the number of records with the same score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- Lists the number of records with the same score
+-- of the table second_table of the database hbtn_0c_0 in MySQL server
+SELECT score, COUNT(*) as number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
