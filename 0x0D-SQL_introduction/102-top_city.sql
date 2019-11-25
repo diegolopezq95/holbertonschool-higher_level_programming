12d5fd91ee8893d15f1d1e1ac8ca449c1571fb96
diegolopezq95 <927@holbertonschool.com>
2019-11-25 19:23:10 +0000

    displays top 3 cities temperature during July and August

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,7 +3,7 @@
 -- of the database hbtn_0c_0 in MySQL server
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
-WHERE MONTH = 7 OR MONTH = 8
+WHERE month = 7 OR month = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
