e29b3e1b5bd9c71699a230d7ff33b0d42723ca05
diegolopezq95 <927@holbertonschool.com>
2019-11-26 19:30:13 +0000

    [Fix]: name of the record fixed

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,4 +6,4 @@ FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 GROUP BY tv_genres.name
-ORDER BY number_shows DESC;
+ORDER BY number_of_shows DESC;
