5467c0918afa8a6c4b74feaf45a27db9f5ff77fe
diegolopezq95 <927@holbertonschool.com>
2019-12-13 17:01:38 +0000

    lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This module lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    users = sys.argv[1]
+    password = sys.argv[2]
+    datab = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=users,
+                         passwd=password, db=datab, charset="utf8")
+""" create an instance of 'cursor' class
+used to execute the 'SQL' statements in 'Python'
+"""
+cursor = db.cursor()
+""" getting records from the table
+"""
+cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
+               INNER JOIN states ON state_id = states.id ORDER BY id ASC")
+""" fetching all records from the 'cursor' object
+"""
+records = cursor.fetchall()
+""" showing the data
+"""
+for row in records:
+    print(row)
+cursor.close()
+db.close()
