9649e9f62a8dbb2155a102589d981a2521619210
diegolopezq95 <927@holbertonschool.com>
2019-12-13 15:47:03 +0000

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,10 @@ if __name__ == "__main__":
     users = sys.argv[1]
     password = sys.argv[2]
     datab = sys.argv[3]
-    db = MySQLdb.connect(host = "localhost", port = 3307, user = users,
-                         passwd = password, db = datab, charset = "utf8")
-""" create an instance of 'cursor' class, used to execute the 'SQL' statements in 'Python'
+    db = MySQLdb.connect(host="localhost", port=3307, user=users,
+                         passwd=password, db=datab, charset="utf8")
+""" create an instance of 'cursor' class
+used to execute the 'SQL' statements in 'Python'
 """
 cursor = db.cursor()
 """ getting records from the table
@@ -20,7 +21,7 @@ cursor.execute("SELECT * FROM states ORDER BY id ASC")
 """
 records = cursor.fetchall()
 """ showing the data
-""" 
+"""
 for row in records:
     print(row)
 cursor.close()
