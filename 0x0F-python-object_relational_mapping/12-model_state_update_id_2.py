9dfee515fa07cc4af17ad2fc32a7c7781c327195
diegolopezq95 <927@holbertonschool.com>
2019-12-13 21:54:34 +0000

    changes the name of a State object with id = 2

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This module changes the name of a State object
+from the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    """Session object provides the entrypoint to acquire a Query object
+    """
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    """query() takes one or more entities and returns a
+    new Query object which will issue mapper queries within
+    the context of this Session
+    """
+    state = session.query(State).filter(State.id == "2")\
+                                .update({State.name: "New Mexico"})
+    session.commit()
+    session.close()
