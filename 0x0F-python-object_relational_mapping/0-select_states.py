bc9513b4ae09634da0fb018aed2bd16f698056bb
diegolopezq95 <927@holbertonschool.com>
2019-12-13 15:51:00 +0000

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This module ists all states from the database hbtn_0e_0_usa
+""" This module lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     users = sys.argv[1]
     password = sys.argv[2]
     datab = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", port=3307, user=users,
+    db = MySQLdb.connect(host="localhost", port=3306, user=users,
                          passwd=password, db=datab, charset="utf8")
 """ create an instance of 'cursor' class
 used to execute the 'SQL' statements in 'Python'
