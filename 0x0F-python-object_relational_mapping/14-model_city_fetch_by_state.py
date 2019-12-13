ed9e40c957d49fd264a94dd15e982cd3108406bd
diegolopezq95 <927@holbertonschool.com>
2019-12-13 23:09:24 +0000

    Class city and print all City objects using join with state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" This module deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    """Session object provides the entrypoint to acquire a Query object
+    """
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    """query() takes one or more entities and returns a
+    new Query object which will issue mapper queries within
+    the context of this Session
+    """
+    for city, state in session.query(City, State)\
+                              .join(State, State.id == City.state_id)\
+                              .order_by(State.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
