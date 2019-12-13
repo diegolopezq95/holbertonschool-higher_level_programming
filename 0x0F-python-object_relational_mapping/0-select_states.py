0cab688a27b200ad485ce7510e704192dfdfb894
diegolopezq95 <927@holbertonschool.com>
2019-12-13 15:43:04 +0000

    lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module .
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    users = sys.argv[1]
+    password = sys.argv[2]
+    datab = sys.argv[3]
+    db = MySQLdb.connect(host = "localhost", port = 3307, user = users,
+                         passwd = password, db = datab, charset = "utf8")
+""" create an instance of 'cursor' class, used to execute the 'SQL' statements in 'Python'
+"""
+cursor = db.cursor()
+""" getting records from the table
+"""
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+""" fetching all records from the 'cursor' object
+"""
+records = cursor.fetchall()
+""" showing the data
+""" 
+for row in records:
+    print(row)
+cursor.close()
+db.close()
