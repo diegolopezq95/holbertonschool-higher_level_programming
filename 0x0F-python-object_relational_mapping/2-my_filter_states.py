a114fa573a469e273c9511470945c31d3092b1a1
diegolopezq95 <927@holbertonschool.com>
2019-12-13 16:09:20 +0000

    takes in an argument and displays all values where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This module takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    users = sys.argv[1]
+    password = sys.argv[2]
+    datab = sys.argv[3]
+    record = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", port=3306, user=users,
+                         passwd=password, db=datab, charset="utf8")
+""" create an instance of 'cursor' class
+used to execute the 'SQL' statements in 'Python'
+"""
+cursor = db.cursor()
+""" getting records from the table
+"""
+save = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(record)
+cursor.execute(save)
+""" fetching all records from the 'cursor' object
+"""
+records = cursor.fetchall()
+""" showing the data
+"""
+for row in records:
+    print(row)
+cursor.close()
+db.close()
