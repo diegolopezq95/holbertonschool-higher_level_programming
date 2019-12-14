4ac3e41f9aecef16757a62dcb09fd69721a05426
diegolopezq95 <927@holbertonschool.com>
2019-12-14 17:24:23 +0000

    lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     """ getting records from the table
     """
-    cursor.execute("SELECT * FROM states WHERE name LIKE \"N%\"\
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY \"N%\"\
                    ORDER BY id ASC")
     """ fetching all records from the 'cursor' object
     """
