e17d5db985e6eaf0cea9be8c21bd8dd6aad284c5
diegolopezq95 <927@holbertonschool.com>
2019-12-14 17:11:03 +0000

    lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,19 +11,19 @@ if __name__ == "__main__":
     datab = sys.argv[3]
     db = MySQLdb.connect(host="localhost", port=3306, user=users,
                          passwd=password, db=datab, charset="utf8")
-""" create an instance of 'cursor' class
-used to execute the 'SQL' statements in 'Python'
-"""
-cursor = db.cursor()
-""" getting records from the table
-"""
-cursor.execute("SELECT * FROM states WHERE name like \"N%\" ORDER BY id ASC")
-""" fetching all records from the 'cursor' object
-"""
-records = cursor.fetchall()
-""" showing the data
-"""
-for row in records:
-    print(row)
-cursor.close()
-db.close()
+    """ create an instance of 'cursor' class
+    used to execute the 'SQL' statements in 'Python'
+    """
+    cursor = db.cursor()
+    """ getting records from the table
+    """
+    cursor.execute("SELECT * FROM states WHERE name like \"N%\" ORDER BY id ASC")
+    """ fetching all records from the 'cursor' object
+    """
+    records = cursor.fetchall()
+    """ showing the data
+    """
+    for row in records:
+        print(row)
+    cursor.close()
+    db.close()
