1a05dc2bbf1aaf4f9af3591b84ca6803ef73d29e
diegolopezq95 <927@holbertonschool.com>
2019-12-14 16:07:50 +0000

    class city and print all City objects using join with state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -20,4 +20,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("state_id"), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
