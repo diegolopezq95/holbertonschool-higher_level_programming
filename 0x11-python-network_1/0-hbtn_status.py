834d88c3bae0ffc1e99ba8eb66c5891888a16ea8
diegolopezq95 <927@holbertonschool.com>
2020-01-13 16:58:18 +0000

    Task 0 fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module fetches https://intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('utf-8'))
