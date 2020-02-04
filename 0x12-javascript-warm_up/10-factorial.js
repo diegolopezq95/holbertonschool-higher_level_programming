f373976b5229f90026f233b007fbc2b0d8713996
diegolopezq95 <927@holbertonschool.com>
2020-02-04 16:25:08 +0000

    prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function factorial (myNumber) {
+  if (isNaN(myNumber) || myNumber === 1 || myNumber === 0) {
+    return 1;
+  } else {
+    return myNumber * factorial(myNumber - 1);
+  }
+}
+var myNumber = parseInt(process.argv[2]);
+console.log(factorial(myNumber));
