ed76eb08dd9e0f84697e6a1e509dcf35d0975040
diegolopezq95 <927@holbertonschool.com>
2020-02-10 19:57:40 +0000

    [Fix]: importing dict directly

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const valueValues = require('./101-data.js').dict;
 const dictOrder = {};
-for (let key in valueValues) {
+for (const key in valueValues) {
   if (!(valueValues[key] in dictOrder)) {
     dictOrder[valueValues[key]] = [];
     dictOrder[valueValues[key]].push(key);
