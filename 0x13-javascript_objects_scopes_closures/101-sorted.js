e3a1ed55b3447cdb7984fcb4ed30c99adc00d29e
diegolopezq95 <927@holbertonschool.com>
2020-02-10 19:38:23 +0000

    imports a dict of occurrences by user id and computes a dict of user ids by occurrence

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const dict = require('./101-data.js');
+const dictValues = Object.values(dict);
+const valueValues = dictValues[0];
+const dictOrder = {};
+for (var key in valueValues) {
+  if (!(valueValues[key] in dictOrder)) {
+    dictOrder[valueValues[key]] = [];
+    dictOrder[valueValues[key]].push(key);
+  } else {
+    dictOrder[valueValues[key]].push(key);
+  }
+}
+console.log(dictOrder);
