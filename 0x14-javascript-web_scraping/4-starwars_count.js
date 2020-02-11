7c1954de54c69b88af75ac024b6183d69ef98dea
diegolopezq95 <927@holbertonschool.com>
2020-02-11 17:42:38 +0000

    prints the number of movies where Wedgeis Antilles is present

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) {
+    throw (error);
+  } else {
+    const resultsList = JSON.parse(body).results;
+    let numFilms = 0;
+    for (const key in resultsList) {
+      const charactList = resultsList[key].characters;
+      for (const keyj in charactList) {
+        if (charactList[keyj] === 'https://swapi.co/api/people/18/') {
+          numFilms += 1;
+        }
+      }
+    }
+    console.log(numFilms);
+  }
+});
