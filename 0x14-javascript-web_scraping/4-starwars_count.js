b91dfc62a5349c62ade53606edfb3c3fc8a8d8de
diegolopezq95 <927@holbertonschool.com>
2020-02-11 18:18:47 +0000

    [Fix]: adding search condition

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const url = process.argv[2];
 const request = require('request');
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
+request(url, function (error, response, body) {
+  if (error) {
+    throw (error);
   } else {
     const resultsList = JSON.parse(body).results;
     let numFilms = 0;
     for (const key in resultsList) {
       const charactList = resultsList[key].characters;
       for (const keyj in charactList) {
-        if (charactList[keyj] === 'https://swapi.co/api/people/18/') {
+        if (charactList[keyj] === 'https://swapi.co/api/people/18/' || charactList[keyj].search('18') > 0) {
           numFilms += 1;
         }
       }
