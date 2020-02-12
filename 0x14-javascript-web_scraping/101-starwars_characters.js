9da0bd9e15a7278b9ce97aa55cc78b90f2082eaf
diegolopezq95 <927@holbertonschool.com>
2020-02-12 03:28:53 +0000

    just trying

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const movieID = process.argv[2];
+const request = require('request');
+const completeUrl = `https://swapi.co/api/films/${movieID}`;
+request(completeUrl, function (error, response, body) {
+  if (error) {
+    throw (error);
+  } else {
+    const filmList = JSON.parse(body).characters;
+    for (const key in filmList) {
+      const chars = filmList[key];
+      request(chars, async function (error, response, body) {
+        if (error) {
+          throw (error);
+        } else {
+          const nameList = JSON.parse(body).name;
+          console.log(nameList);
+        }
+      });
+    }
+  }
+});
